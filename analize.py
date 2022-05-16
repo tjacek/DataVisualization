e3a5bb1d8dfa299f70f964fe36c0e8eb335c4466
tjacek <tjacek@agh.edu.pl>
2022-05-16 02:28:25 +0200

    show plot

--- analize.py
+++ analize.py
@@ -1,15 +1,11 @@
 #import arff
 #import numpy as np
-#import reduction
-#import matplotlib.pyplot as plt
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 #from mpl_toolkits.mplot3d import Axes3D
 from sklearn.decomposition import PCA
 #from numpy import linalg as LA
 #from sklearn.lda import LDA
 import pandas as pd 
-
-colors=['b','g','r','y']
 
 def show_data(in_path):
     df = pd.read_csv(in_path, sep='\s+')
@@ -21,7 +17,15 @@ def show_data(in_path):
     print(pca.components_)
     print(pca.explained_variance_ratio_)
     print(x_t)
-    
+    plot(names,x_t)
+
+def plot(names,data):
+    fig, ax = plt.subplots()
+    ax.scatter(data[:,0],data[:,1])
+    for i,txt in enumerate(names):
+        ax.annotate(txt,data[i])
+    plt.show()
+
 #def showDataset():
 #    dataset=arff.readArffDataset("innovation_.arff")     
 #    x,y=apply_MDA(dataset.data)
@@ -46,15 +50,6 @@ def show_data(in_path):
 #    ax.scatter(x,y,z,c=c_p)
 #    for i,txt in enumerate(ccodes):
 #        ax.annotate(txt, (x[i],y[i],z[i]))
-
-#def apply_PCA(X):
-#    pca = PCA(n_components=3)
-#    x_t=pca.fit(X).transform(X)
-#    cor1=[x_t[i][0] for i in xrange(len(x_t)) ]
-#    cor2=[x_t[i][1] for i in xrange(len(x_t)) ]
-#    print(pca.components_)
-#    print(pca.explained_variance_ratio_) 
-#    return cor1,cor2
 
 #def apply_LDA(X,Y):
 #    clf = LDA(n_components=2)
