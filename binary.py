dde59873cb86127bb16e2c7102a5ebc9c719124f
tjacek <tjacek@agh.edu.pl>
2022-05-22 16:36:19 +0200

    extract vector

--- binary.py
+++ binary.py
@@ -1,8 +1,21 @@
+import numpy as np
 import json
 
 class BinaryExtractor(object):
     def __init__(self,prop_ids):
         self.prop_ids=prop_ids
+
+    def __len__(self):
+        return len(self.prop_ids)	
+
+    def __call__(self,instance):
+        if(type(instance)==dict):
+            return { key_i:self(value_i) 
+                for key_i,value_i in instance.items()}	
+        features=np.zeros((len(self),))
+        for inst_i in instance:
+            features[self.prop_ids[inst_i]]=1
+        return features	
 
 def from_json(in_path):
     with open(in_path) as json_file:
@@ -17,4 +30,6 @@ def make_extractor(raw_dict):
     prop_ids={prop_i:i  for i,prop_i in enumerate(list(all_properties))}
     return BinaryExtractor(prop_ids)
 
-make_extractor('adom')
+raw_dict= from_json('adom')
+extractor=make_extractor(raw_dict)
+print(extractor(raw_dict))
