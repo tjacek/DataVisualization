78d8a6e2f1b5ef85b734fa36f5c2022e790aff52
tjacek <tjacek@agh.edu.pl>
2024-08-15 01:57:11 +0200

    pca

--- utils.py
+++ utils.py
@@ -1,13 +1,31 @@
 import pandas as pd
 from sklearn.decomposition import PCA
 
+class Dataset(object):
+    def __init__(self,X,y):
+        self.X=X
+        self.y = y
+
+    def n_cats(self):
+        return int(max(self.y))
+
+    def get_cat(self,i):
+    	return self.X[self.y==i]
+		
+
 def read_csv(in_path:str):
     df=pd.read_csv(in_path)
-    X=df.to_numpy()
+    raw=df.to_numpy()
+    X,y=raw[:,:-1],raw[:,-1]
+    return Dataset(X,y)
+
+def get_pca(X,y=None):
     pca = PCA()#n_components=2)
     pca.fit(X)
     print(pca.explained_variance_ratio_)
-#    print(X.shape)
 
 if __name__ == '__main__':
-	read_csv("uci/cleveland")
+    data=read_csv("uci/cleveland")
+    for i in range(data.n_cats()):
+        x_i=data.get_cat(i)
+        get_pca(x_i)
