6468aeea0a837ca82711f48090030103c7ed7ef3
tjacek <tjacek@agh.edu.pl>
2024-08-19 01:07:47 +0200

    eval dataset

--- utils.py
+++ utils.py
@@ -16,6 +16,15 @@ class Dataset(object):
         return Dataset(X=fun(self.X),
                        y=self.y)
 
+    def eval(self,train_index,test_index,clf):
+        X_train=self.X[train_index]
+        y_train=self.y[train_index]
+        X_test=self.X[test_index]
+        y_test=self.y[test_index]
+        clf.fit(X_train,y_train)
+        y_pred=clf.predict(X_test)
+        return y_pred,y_test
+
 def read_csv(in_path:str):
     df=pd.read_csv(in_path)
     raw=df.to_numpy()
