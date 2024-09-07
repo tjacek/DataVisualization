6f6f8055d2fefe6feef84e6d67ca76cbd38351d6
tjacek <tjacek@agh.edu.pl>
2024-09-08 00:47:01 +0200

    randomize color

--- plot.py
+++ plot.py
@@ -1,13 +1,20 @@
+import numpy as np
 import matplotlib.pyplot as plt
 import dataset
 
 def plot(data):
     if(data.dim()!=2):
-        raise Exception(f"dim of data:{data.dim()}")
-    else:
-        fig, ax = plt.subplots()
-        ax.scatter(data.X[:,0],data.X[:,1])
-        plt.show()
+        raise Exception(f"dim of data:{data.dim()}")    
+    fig, ax = plt.subplots()
+    ax.scatter(data.X[:,0],data.X[:,1])
+    cat2col= np.arange(20)
+    np.random.shuffle(cat2col)
+    if(data.labeled()):
+       for i,y_i in enumerate(data.y):
+           color_i=cat2col[int(y_i)]
+           ax.annotate(y_i,data.X[i],
+                    color=plt.cm.tab20(color_i))
+    plt.show()
 
 #def plot(data_dict):
 #    names,data,y=data_dict.to_dataset()
@@ -22,7 +29,7 @@ def plot_supervised(names,X,y):
     ax.scatter(X[:,0],X[:,1])
     for i,txt in enumerate(names):
         print(txt)
-        ax.annotate(txt,X[i],color=plt.cm.tab20(2*y[i]))
+        ax.annotate(txt,data.X[i],color=plt.cm.tab20(2*y[i]))
     plt.show()
 
 def plot_unsupervised(names,X):
