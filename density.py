1bb570f95735ecf2cdbef2ba02b463cd2414f4ac
tjacek <tjacek@agh.edu.pl>
2024-09-11 23:57:34 +0200

    show matrix

--- density.py
+++ density.py
@@ -3,6 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.neighbors import KernelDensity
 from scipy.special import kl_div
+import seaborn as sns
+
 import dataset
 
 def compute_density(data,dim,cat=None,show=False,n_steps=100):
@@ -16,7 +18,6 @@ def compute_density(data,dim,cat=None,show=False,n_steps=100):
     delta= (a_max-a_min)/n_steps
     x_order=np.arange(n_steps)*delta
     x_order-=a_min
-#    x_order= np.sort(np.unique(x_i))
     log_dens= kde.score_samples(x_order.reshape(-1, 1))
     dens=np.exp(log_dens)
 #    dens=log_dens
@@ -31,8 +32,6 @@ def kl_matrix(data,cat_i=0):
     matrix=[]
     all_dens=[ compute_density(data,dim_j,cat_i) 
         for dim_j in range(n_dims)]
-    print([d.shape for d in all_dens])
-
     for dens_i in all_dens:
         matrix.append([])
         for dens_j in all_dens:
@@ -40,7 +39,10 @@ def kl_matrix(data,cat_i=0):
             matrix[-1].append(kl_ij)
     matrix=np.array(matrix)
     print(np.around(matrix,decimals=2))
-    
+    plt.figure(figsize = (10,7))
+    sns.heatmap(matrix, annot=True)
+    plt.show()
+
 if __name__ == '__main__':
     data=dataset.read_csv("uci/cleveland")
     kl_matrix(data)
