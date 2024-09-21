2f4b022be1a7aab557f1c1265c4e6d424f8a7d8f
tjacek <tjacek@agh.edu.pl>
2024-09-21 23:28:44 +0200

    save plot

--- plot.py
+++ plot.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import dataset,reduction
+import dataset,reduction,utils
 
-def plot(data):
+def plot(data,show=True):
     if(data.dim()!=2):
         raise Exception(f"dim of data:{data.dim()}")    
     fig, ax = plt.subplots()
@@ -14,19 +14,22 @@ def plot(data):
            color_i=cat2col[int(y_i)]
            ax.annotate(y_i,data.X[i],
                     color=plt.cm.tab20(color_i))
-    plt.show()
+    if(show):
+        plt.show()
 
-def reduce_plots(in_path,transform=None):
+def reduce_plots(in_path,out_path,transform=None):
     data=dataset.read_csv(in_path)
     if(transform is None):
         transform={ "pca":dataset.get_pca,
                   "spectral":reduction.spectral_transform}
+    utils.make_dir(out_path)
     for name_i,transform_i in transform.items():
         data_i=transform_i(data,n_components=2)
         plot(data_i)
+        plt.savefig(f'{out_path}/{name_i}')
 
 if __name__ == '__main__':
-    reduce_plots("uci/cleveland",transform=None)
+    reduce_plots("uci/cleveland","test",transform=None)
 #    data=dataset.read_csv("uci/cleveland")
 #    data=dataset.get_pca(data,n_components=2)
 #    plot(data)
