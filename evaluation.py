a54e61df7c3778e31338cb6856102242774c6999
tjacek <tjacek@agh.edu.pl>
2024-10-08 10:02:46 +0200

    MeticDict

--- evaluation.py
+++ evaluation.py
@@ -5,17 +5,34 @@ from sklearn.metrics import accuracy_score,balanced_accuracy_score
 import pandas as pd
 import exp
 
+class MetricDict(object):
+    def __init__(self):
+        self.metrics={"acc":accuracy_score,
+                      "balance":balanced_accuracy_score}
+        self.dicts={ metric_i:defaultdict(lambda : [])  
+                        for metric_i in self.metrics}
+
+    def __call__(self,result_dict):
+        for id_i,results_i in result_dict.items():
+            for name_j,metric_j in self.metrics.items():
+                metric_values=[result_k.get_metric(metric_j) 
+        	                for result_k in results_i]
+                self.dicts[name_j][id_i]=metric_values 	
+        return self
+
+    def keys(self):
+    	return self.dicts["acc"].keys()
+
 def basic_summary(in_path:str):
     result_dict=read_results(in_path)
-    metrics=[accuracy_score,balanced_accuracy_score]
+    metrict_dict=MetricDict()(result_dict)
     lines=[]
-    for id_i,results_i in result_dict.items():
+    for id_i in metrict_dict.keys():
         line_i=id_i.split("/")[-3:]
-        for metric_j in metrics:
-        	metric_values=[result_k.get_metric(metric_j) 
-        	                for result_k in results_i]
-        	line_i.append(np.mean(metric_values))
-        	line_i.append(np.std(metric_values))
+        for metric_j in metrict_dict.metrics:
+            values=metrict_dict.dicts[metric_j][id_i]
+            line_i.append( np.mean(values))
+            line_i.append( np.std(values))
         lines.append(line_i)
     df=pd.DataFrame.from_records(lines)
     return df	
