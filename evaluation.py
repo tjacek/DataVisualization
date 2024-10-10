313aed7e94654933a82a0a048adcb19d8cc4519a
tjacek <jacekt89@gmail.com>
2024-10-10 20:47:31 +0200

    compare ids

--- evaluation.py
+++ evaluation.py
@@ -16,11 +16,11 @@ class MetricDict(object):
         self.cols=["data","feat","clf","key"]
 
     def __call__(self,result_dict):
-        for id_i,results_i in result_dict.items():
+        for path_i,results_i in result_dict.items():
             for name_j,metric_j in self.metrics.items():
-                metric_values=[result_k.get_metric(metric_j) 
+                metric_values=[100*result_k.get_metric(metric_j) 
         	                for result_k in results_i]
-                self.dicts[name_j][id_i]=metric_values 	
+                self.dicts[name_j][get_id(path_i)]=metric_values 	
         return self
 
     def metric_name(self):
@@ -32,7 +32,8 @@ class MetricDict(object):
     def key_frame(self):
         lines=[]
         for key_i in self.keys():
-            line_i=key_i.split('/')[-3:]
+#            print(key_i)
+            line_i=key_i.split(',')#key_i.split('/')[-3:]
             line_i.append(key_i)
             lines.append(line_i)
         return pd.DataFrame.from_records(lines,
@@ -53,9 +54,6 @@ def basic_summary(in_path:str):
     return df	
 
 def stat_test(in_path,query=None):
-    if(query is None):
-        query={"clf":"RF"}
-    query_str=' '.join([ f"& {col_i}=='{value_i}'" for col_i,value_i in query.items()])
     result_dict=read_results(in_path)
     metrict_dict=MetricDict()(result_dict)
     cols=["id_x","id_y",]
@@ -64,16 +62,17 @@ def stat_test(in_path,query=None):
                     for col in ["diff","pvalue","sig"]]
     lines=[]
     kf=metrict_dict.key_frame()
+    query_fun= get_query_fun(kf,query)
     for data_k in kf["data"].unique():
-        kf_query=kf.query(f"data=='{data_k}' {query_str}") #& clf==RF)
-        valid_id=list(kf_query['key'])
-        for x,y in itertools.combinations(valid_id, 2):
-            id_x=get_id(x)
-            id_y=get_id(y)
+        valid_id= query_fun(data_k)
+#        valid_id=list(kf_query['key'])
+        for id_x,id_y in itertools.combinations(valid_id, 2):
+
             line=[id_x,id_y]
+            print(line)
             for metric_i in metrict_dict.metric_name():
-                x_metric=metrict_dict.dicts[metric_i][x]
-                y_metric=metrict_dict.dicts[metric_i][y]
+                x_metric=metrict_dict.dicts[metric_i][id_x]
+                y_metric=metrict_dict.dicts[metric_i][id_y]
                 pvalue=stats.ttest_ind(x_metric,y_metric,
                                        equal_var=False)[1]
                 diff=np.mean(x_metric)-np.mean(y_metric)
@@ -83,6 +82,20 @@ def stat_test(in_path,query=None):
     df=pd.DataFrame.from_records(lines,
                                  columns=cols)
     return df
+
+def get_query_fun(kf,query):
+    if(query is None):
+        query={"clf":"RF"}
+    if(type(query)==list):
+        def fun(data_k):
+            return [ f"{data_k},{q_i}" for q_i in query]
+        return fun
+    query_str=' '.join([ f"& {col_i}=='{value_i}'" 
+                           for col_i,value_i in query.items()])
+    def fun(data_k):
+        kf_query= kf.query(f"data=='{data_k}' {query_str}") 
+        return list(kf_query['key'])
+    return fun
 
 def read_results(in_path:str):
     result_dict=defaultdict(lambda :[])
@@ -96,5 +109,5 @@ def read_results(in_path:str):
 def get_id(path:str):
     return ",".join( path.split('/')[-3:])
 
-df=stat_test("deep",{'feat':'base'})
+df=stat_test("deep",['deep,LR','base,RF'])
 print(df)
