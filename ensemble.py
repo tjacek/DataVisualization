2f255f4b0d146f5da2e81b54ff2cef3567afd219
tjacek <tjacek@agh.edu.pl>
2024-10-28 02:30:29 +0100

    gauss ensemble

--- ensemble.py
+++ ensemble.py
@@ -13,23 +13,31 @@ class Ensemble(object):
         return np.argmax(votes,axis=1) 
 
 class GaussEnsemble(Ensemble):
-	def __init__(self,k, verbose=0):
+	def __init__(self,k,full=True, verbose=0):
 		super().__init__()
 		self.k=k
+		self.full=full
 		self.verbose=verbose 
 	
 	def fit(self,X,y):
 		clustering=gauss.gaussian_clustering((X,y),k=self.k)
 		hist=clustering.hist()
-		print(hist.arr)
-		raise Exception(hist.recall_matrix())
+#		print(hist.arr)
+		recall=hist.recall_matrix()
 		n_cats=clustering.dataset.n_cats()
-		for k in range(clustering.n_clusters()):
-			cls_k=clustering.wihout_cluster(k)
-			nn_k=deep.ClfCNN(default_cats=n_cats,
-				             verbose=self.verbose)
-			nn_k.fit(X=cls_k.X,y=cls_k.y)
-			self.clfs.append(nn_k)
+		for k,recall_k in enumerate(recall.T):#clustering.n_clusters()):
+			if(np.all(recall_k<0.5)):
+				cls_k=clustering.wihout_cluster(k)
+				nn_k=deep.ClfCNN(default_cats=n_cats,
+								 verbose=self.verbose)
+				nn_k.fit(X=cls_k.X,y=cls_k.y)
+				self.clfs.append(nn_k)
+		if(self.full):
+			nn=deep.ClfCNN(default_cats=n_cats,
+						   verbose=self.verbose)
+			nn.fit(X=clustering.dataset.X,
+				   y=clustering.dataset.y)
+			self.clfs.append(nn)
 
 @utils.elapsed_time
 def compare_ensemble(in_path,deep_ens=None,verbose=0):
@@ -40,14 +48,14 @@ def compare_ensemble(in_path,deep_ens=None,verbose=0):
     nn=deep.ClfCNN(verbose=verbose)
     gen=exp.simple_split(data,n_splits=10)
     train,test=next(gen)
-#    result_nn=data.eval(train,test,nn)
+    result_nn=data.eval(train,test,nn)
     result_ens=data.eval(train,test,deep_ens)
     return result_nn,result_ens
 
 if __name__ == '__main__':
     nn,ens=compare_ensemble("uci/cleveland",
     	                    verbose=0)
-    nn.report()
-    ens.report()
-#    print(nn.get_acc())
-#    print(ens.get_acc())
+#    nn.report()
+#    ens.report()
+    print(nn.get_acc())
+    print(ens.get_acc())
