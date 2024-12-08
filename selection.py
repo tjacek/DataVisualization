d657a098ba9132f91e34f5c5a775b174c2843854
tjacek <jacekt89@gmail.com>
2024-12-08 19:42:16 +0100

    find_disc

--- selection.py
+++ selection.py
@@ -1,17 +1,35 @@
 import numpy as np
 from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.feature_selection import SelectFromModel
+#from sklearn.feature_selection import RFE
+from sklearn.linear_model import LogisticRegression
 import pandas as pd
 import dataset,utils
+
+def find_disc(in_path):
+    df=pd.read_csv(in_path)
+    y=df['target'].to_numpy()
+    X=df[df.columns.difference(['data','target'])]
+    X=X.to_numpy()
+    clf=LogisticRegression()
+    clf.fit(X,y)
+    y_pred=clf.predict(X)
+    print(y)
+    print(y_pred)
 
 def feat_comp(in_path):
     result_dict=tree_impor(in_path,verbose=False)
     gini_dict={ data_i.split('/')[-1]: gini(arr_i) 
              for  data_i,arr_i in result_dict.items()}
+    
+    for data_i,arr_i in result_dict.items():
+        print(data_i)
+#        print(arr_i)
+        print(np.cumsum(np.flip(arr_i)))
     df=pd.DataFrame.from_records(data=list(gini_dict.items()),
     	                         columns=['data','gini'])
     df=df.sort_values(by='gini')
-    print(df)
+#    print(df)
 
 @utils.DirFun({'in_path':0})
 def tree_impor(in_path,verbose=True):
@@ -32,4 +50,4 @@ def gini(x):
         total += np.sum(np.abs(xi - x[i:]))
     return total / (len(x)**2 * np.mean(x))
 
-feat_comp("../uci")  
+find_disc(in_path="purity.csv")
